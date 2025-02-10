73a38170df70e93a31fecfcf19911284c1d60e63
Prabhash Mac <gcpara8@gmail.com>
2025-02-10 12:53:02 -0600

    added Navigation Property for TicketInventory

--- Models/GasStation.cs
+++ Models/GasStation.cs
@@ -17,5 +17,7 @@ namespace LottoManager.Models{
         public string Email { get; set; } = string.Empty;
 
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public List<TicketInventory> TicketInventorys {get; set;} = new();
     }
 }
